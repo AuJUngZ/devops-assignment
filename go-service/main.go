4fb22b468f2f654c3699fe4e7982a35773690244
AuJung <nuttapong6280@gmail.com>
2025-04-22 20:21:24 +0700

    feat: enhance go service to show how kustomize work feat: add kustomize and kube config

--- go-service/main.go
+++ go-service/main.go
@@ -12,7 +12,7 @@ func main() {
 	http.HandleFunc("/", getHello)
 	err := http.ListenAndServe(":8080", nil)
 	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("server closed\n")
+		fmt.Println("server closed")
 	} else if err != nil {
 		fmt.Printf("error starting server: %s\n", err)
 		os.Exit(1)
@@ -21,5 +21,26 @@ func main() {
 
 func getHello(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
-	io.WriteString(w, "Hello "+name+"\n")
+	if name == "" {
+		name = "Guest"
+	}
+
+	appName := os.Getenv("APP_NAME")
+	testEnv := os.Getenv("TEST_ENV")
+
+	missing := ""
+	if appName == "" {
+		missing += "APP_NAME, "
+	}
+	if testEnv == "" {
+		missing += "TEST_ENV "
+	}
+
+	if missing != "" {
+		io.WriteString(w, fmt.Sprintf("Missing environment variable(s): %s\n", missing))
+		return
+	}
+
+	response := fmt.Sprintf("Hello %s from %s (%s)\n", name, appName, testEnv)
+	io.WriteString(w, response)
 }
